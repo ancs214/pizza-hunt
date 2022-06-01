b011b29f7a07dd77a81c8b631b160ef06c86d507
ancs214 <ancs214@gmail.com>
2022-06-01 14:11:27 -0400

    associated the pizza and comment models. added virtual to pizza model to get total count of comments

--- models/pizza.js
+++ models/pizza.js
@@ -2,24 +2,45 @@ const { Schema, model } = require('mongoose');
 
 //CREATE MONGOOSE SCHEMA
 const PizzaSchema = new Schema({
-    pizzaName: {
-      type: String
+  pizzaName: {
+    type: String
+  },
+  createdBy: {
+    type: String
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  },
+  size: {
+    type: String,
+    default: 'Large'
+  },
+  // [] indicates an array as the data type
+  toppings: [],
+  //we need to tell Mongoose to expect ObjectId and that the data comes from the Comment model
+  comments: [
+    {
+      type: Schema.Types.ObjectId,
+      //ref tells the Pizza model which doc to search to find the right comments
+      ref: 'Comment'
+    }
+  ]
+},
+  //tell schema to use virtuals
+  {
+    toJSON: {
+      virtuals: true,
     },
-    createdBy: {
-      type: String
-    },
-    createdAt: {
-      type: Date,
-      default: Date.now
-    },
-    size: {
-      type: String,
-      default: 'Large'
-    },
-    // [] indicates an array as the data type
-    toppings: []
-  });
-  
+    id: false
+  }
+);
+
+// get total count of comments and replies on retrieval
+PizzaSchema.virtual('commentCount').get(function () {
+  return this.comments.length;
+});
+
 // create the Pizza model using the PizzaSchema
 const Pizza = model('Pizza', PizzaSchema);
 
