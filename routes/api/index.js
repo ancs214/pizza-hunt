2d27b2a536fd58b887437dcb1c07375633cd66c6
ancs214 <ancs214@gmail.com>
2022-06-03 18:08:38 -0400

    fixed route in index js file

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const pizzaRoutes = require('-routes');
+const pizzaRoutes = require('./pizza-routes');
 const commentRoutes = require('./comment-routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
