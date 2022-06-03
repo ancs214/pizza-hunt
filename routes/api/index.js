1cbbc9300c905a0f9e4dcd1a9fd85d36c5a12d27
ancs214 <ancs214@gmail.com>
2022-06-03 18:02:54 -0400

    changed capitalization in index js file after CANNOT FIND MODULE error from heroku

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const pizzaRoutes = require('./pizza-routes');
+const pizzaRoutes = require('-routes');
 const commentRoutes = require('./comment-routes');
 
 // add prefix of `/pizzas` to routes created in `pizza-routes.js`
